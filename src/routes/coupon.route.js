374683de85ff06cb842b03440a7647782dbe348e
abhijth.r.b <abhijithrb91@gmail.com>
2025-02-27 13:47:29 +0530

    coupon validation

--- src/routes/coupon.route.js
+++ src/routes/coupon.route.js
@@ -2,6 +2,7 @@ const { calculateCouponCtrl, fetchAvailableCouponsCtrl, fetchAllCouponsCtrl, fet
 const { authMiddleware } = require("../middlewares/auth.middleware");
 const { roleChecker } = require("../middlewares/roleChecker.middleware");
 const { validate } = require("../middlewares/validate.middleware");
+const { couponValidator } = require("../validators/coupon.validator");
 
 const couponRouter = require("express").Router();
 
@@ -11,10 +12,10 @@ couponRouter.post("/calculate", calculateCouponCtrl);
 couponRouter.get("/available", roleChecker(['user']), fetchAvailableCouponsCtrl);
 
 couponRouter.get("/all", roleChecker(['admin']), fetchAllCouponsCtrl);
-couponRouter.get("/:id", fetchCouponByIdCtrl );
+couponRouter.get("/:id", fetchCouponByIdCtrl);
 
 couponRouter.use(roleChecker(['admin']))
-couponRouter.post("", createCouponCtrl);
-couponRouter.put("/:id", updateCouponCtrl);
+couponRouter.post("", couponValidator.create, validate, createCouponCtrl);
+couponRouter.put("/:id", couponValidator.update, validate, updateCouponCtrl);
 
 module.exports = { couponRouter }
