eb5ef013eabb04899be03e8c15bd80412d7fe02b
abhijth.r.b <abhijithrb91@gmail.com>
2025-03-29 19:33:39 +0530

    delhivery API services

--- src/controllers/webhook.controller.js
+++ src/controllers/webhook.controller.js
@@ -1,5 +1,6 @@
-const { verifyPhonePeHash, checkoutOrderCompletedHandler, 
-    checkoutOrderFailedHandler, pgRefundCompletedHandler, 
+const { deliveryConfirmation } = require("../services/logistics.service");
+const { verifyPhonePeHash, checkoutOrderCompletedHandler,
+    checkoutOrderFailedHandler, pgRefundCompletedHandler,
     pgRefundAcceptedHandler,
     pgRefundFailedHandler
 } = require("../services/webhook.service");
@@ -27,16 +28,42 @@ module.exports.phonepeWebhookHandler = async (req, res) => {
         else if (event === "checkout.order.failed" && payload.state === "FAILED") {
             await checkoutOrderFailedHandler(payload)
         }
-        else if(event === "pg.refund.accepted" && payload.state === "CONFIRMED"){
+        else if (event === "pg.refund.accepted" && payload.state === "CONFIRMED") {
             await pgRefundAcceptedHandler(payload)
         }
-        else if(event === "pg.refund.completed" && payload.state === "COMPLETED"){
+        else if (event === "pg.refund.completed" && payload.state === "COMPLETED") {
             await pgRefundCompletedHandler(payload)
         }
-        else if(event === "pg.refund.failed" && payload.state === "FAILED"){
+        else if (event === "pg.refund.failed" && payload.state === "FAILED") {
             await pgRefundFailedHandler(payload)
         }
 
+
+        return res.status(200).json({
+            success: true,
+            message: "success",
+            data: null,
+            error: null,
+        })
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            success: false,
+            message: "Internal Server error",
+            data: null,
+            error: 'INTERNAL_SERVER_ERROR'
+        })
+    }
+}
+
+
+module.exports.delhiveryWebhookHandler = async (req, res) => {
+    try {
+        const { waybill, status, delivered_on } = req.body;
+
+        if (status === "Delivered") {
+            await deliveryConfirmation(waybill, delivered_on)
+        }
 
         return res.status(200).json({
             success: true,
--- src/services/logistics.service.js
+++ src/services/logistics.service.js
@@ -1,10 +1,19 @@
 const { default: axios } = require("axios");
+const { Order } = require("../models/order.model");
 
 const NODE_ENV = process.env.NODE_ENV;
 const isProduction = NODE_ENV === "production";
 
 const base_url = isProduction ? process.env.lp_prod_base_url : process.env.lp_test_base_url;
 const token = process.env.lp_access_token
+
+const pickup_location = {
+    name: "Merchant Name",
+    pin: "560001",
+    city: "Bangalore",
+    phone: "9876543210",
+    address: "Merchant Address"
+}
 
 const delhiveryApi = axios.create({
     baseURL: base_url,
@@ -17,17 +26,105 @@ delhiveryApi.interceptors.request.use(async (config) => {
     return config;
 })
 
+// API Call Order
+// 1️. Serviceability Check (to verify delivery availability)
+// 2️. Create Shipment (to generate waybill)
+// 3️. Generate Shipping Label (print & attach label)
+// 4️. Schedule Pickup (courier picks up the package)
+// 5️. Track Shipment (real-time updates)
+// 6️. Delivery Confirmation (receive webhook notification)
+
 module.exports.getPincodeServicibility = async (pincode) => {
     const response = await delhiveryApi.get(`/c/api/pin-codes/json/?token=${token}&filter_codes=${pincode}`)
 
+    if (!response?.data) {
+        throw new Error('Failed')
+    }
 
+    const deliveryCode = response?.data?.delivery_codes[0]
+    const codAvailabilty = deliveryCode?.cod;
+    const prepaidAvailabilty = deliveryCode?.pre_paid;
+
+    if (codAvailabilty || prepaidAvailabilty) {
+        return true;
+    }
+
+    return false;
 }
 
 
 module.exports.calculateShippingCost = async (params) => {
     const { md, cgm, o_pin, d_pin, ss } = params
-    
+
     const response = await delhiveryApi.get(`/api/kinko/v1/invoice/charges/.json?md=${md}&cgm=${cgm}&o_pin=${o_pin}&d_pin=${d_pin}&ss=${ss}`)
 
+    if (isNaN(response?.data?.Total_amount)) {
+        throw new Error('Invalid Value')
+    }
 
+    return response?.data?.Total_amount;
+}
+
+
+module.exports.createAShipment = async (shipment) => {
+    const dataObj = {
+        pickup_location,
+        shipments: [shipment]
+    }
+
+    const response = await delhiveryApi.post("/api/cmu/create.json", dataObj)
+
+    return response.data
+}
+
+module.exports.updateAShipment = async (shipment) => {
+    const dataObj = {
+        pickup_location,
+        shipments: [shipment]
+    }
+
+    const response = await delhiveryApi.post("/api/p/edit", dataObj)
+
+    return response.data
+}
+
+module.exports.cancelAShipment = async () => {
+    const response = await delhiveryApi.post("/api/p/edit", { cancellation: true })
+
+    return response.data
+}
+
+
+module.exports.genShipLabel = async (waybill) => {
+    const response = await delhiveryApi.get(`/api/p/packing_slip?wbns=${waybill}&pdf=true`)
+
+    return response.data;
+}
+
+module.exports.schedulePickup = async (shipments) => {
+    const dataObj = {
+        pickup_location,
+        shipments
+    }
+
+    const response = await delhiveryApi.post(`/fm/request/new/`, dataObj)
+
+    return response.data
+}
+
+module.exports.trackShipStatus = async (waybill) => {
+    const response = await delhiveryApi.get(`/api/v1/packages/json/?waybill=${waybill}&token=${token}`)
+
+    return response.data;
+}
+
+// Service for Webhook Controller 
+module.exports.deliveryConfirmation = async (waybill, delivered_on) => {
+    await Order.findOneAndUpdate({ waybill }, {
+        $set: {
+            status: 'delivered',
+            waybill,
+            delivered_on
+        }
+    })
 }
