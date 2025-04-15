c425fb941f2599b77b59f0aeacf3c1e51474d149
abhijth.r.b <abhijithrb91@gmail.com>
2025-04-15 16:02:14 +0530

    review service name bug fix

--- src/controllers/review.controller.js
+++ src/controllers/review.controller.js
@@ -1,5 +1,5 @@
 const { isValidObjectId } = require("mongoose");
-const { createReview, updateReview, getReviewById, getManyReviews, deleteReview } = require("../services/review.service.js");
+const { createReview, updateReview, getReviewById, getManyReviews, deleteReview } = require("../services/reviews.service.js");
 
 module.exports.createReviewCtrl = async (req, res) => {
     try {
